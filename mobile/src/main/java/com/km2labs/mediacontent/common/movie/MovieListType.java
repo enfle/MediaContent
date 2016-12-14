cc73f68867b253cdc17daeecb66ebe6553ccc38f
Subham Tyagi <subhamtyagi@km2labs.com>
2016-12-14 21:05:00 +0530

    Refactoring Phase 3

--- mobile/src/main/java/com/km2labs/mediacontent/common/movie/MovieListType.java
+++
@@ -1,9 +0,0 @@
-package com.km2labs.mediacontent.common.movie;
-
-public enum MovieListType {
-    LATEST,
-    POPULAR,
-    UPCOMING,
-    NOW_PLAYING,
-    TOP_RATED
-}
--- mobile/src/main/java/com/km2labs/mediacontent/common/utils/Logger.java
+++
@@ -1,9 +0,0 @@
-package com.km2labs.mediacontent.common.utils;
-
-/**
- * Created by : Subham Tyagi
- * Created on :  25/10/16.
- */
-
-public class Logger {
-}
