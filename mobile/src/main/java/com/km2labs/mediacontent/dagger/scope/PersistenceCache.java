f8cb389f4763fe29043955285ad51ce25a0135bd
Subham Tyagi <shubham.k.tyagi@gmail.com>
2017-12-13 13:59:38 +0530

    adding missing files

---
+++ mobile/src/main/java/com/km2labs/mediacontent/dagger/scope/PersistenceCache.java
@@ -0,0 +1,19 @@
+package com.km2labs.mediacontent.dagger.scope;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+
+import javax.inject.Qualifier;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Created by : Subham Tyagi
+ * Created on :  02/09/16.
+ */
+
+@Qualifier()
+@Documented
+@Retention(RUNTIME)
+public @interface PersistenceCache {
+}
